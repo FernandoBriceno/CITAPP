ab795e68c2ad2bb4cddb6075965a564a35ce28d7
Fer <luferbrica@gmail.com>
2021-12-06 15:02:55 -0500

    Se crea la entidad Usuario

---
+++ proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Usuario.java
@@ -0,0 +1,47 @@
+package com.misiontic.citapp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="usuario")
+public class Usuario {
+	@Id
+	@Column(name="idusuario", nullable=false)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idUsuario;
+	
+	@Column(name="idrol", nullable=false)
+	private Long idRol;
+	
+	@Column(name="clave", nullable=false, length=20)
+	private String clave;
+	
+	@Column(name="identificacion", nullable=false)
+	private Long identificacion;
+	
+	@Column(name="nombreusuario", nullable=false, length=200)
+	private String nombreUsuario;
+	
+	@Column(name="direccion", nullable=false, length=200)
+	private String direccion;
+	
+	@Column(name="telefono", nullable=false)
+	private Long telefono;
+	
+	@Column(name="correo", nullable=false, length=200)
+	private String correo;
+	
+	public Usuario(){
+	}
+
+}
