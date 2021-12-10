7a717f1afb7e3b665d8fcfe977cfaabdfbd4f753
Fer <luferbrica@gmail.com>
2021-12-10 11:06:13 -0500

    Ya listo para primera version

--- proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Cita.java
+++ proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Cita.java
@@ -3,8 +3,6 @@ package com.misiontic.citapp.entity;
 import java.sql.Time;
 import java.time.LocalDate;
 
-import javax.management.relation.Role;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -13,10 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
