0ee34346828c1119dc03483abd3fb72e0e61ea09
Fer <luferbrica@gmail.com>
2021-12-10 10:55:24 -0500

    Se modificó entidad Medico

--- proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Medico.java
+++ proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Medico.java
@@ -1,13 +1,23 @@
 package com.misiontic.citapp.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
@@ -25,7 +35,7 @@ public class Medico {
 	private Long idMedico;
 	
 	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "idUsuario", nullable = false, updatable = false, referencedColumnName = "idUsuario")
+	@JoinColumn(name = "idusuario", nullable = false, updatable = false, referencedColumnName = "idusuario")
 	private Usuario idUsuario;
 	
 	@Column(name = "especialidad", nullable = false, length = 200)
@@ -35,6 +45,6 @@ public class Medico {
 	private String jornada;
 	
 	@OneToMany(mappedBy = "idMedico", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	private List<Citas> listaCitas;
+	private List<Cita> listaCitas;
 
 }
