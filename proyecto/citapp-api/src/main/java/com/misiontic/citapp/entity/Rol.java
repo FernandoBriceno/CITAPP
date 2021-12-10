9fb20ce3d02543d39be97a5024c26d1ed8a593ee
Fer <luferbrica@gmail.com>
2021-12-10 11:04:06 -0500

    Se modificó entidad Rol

--- proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Rol.java
+++ proyecto/citapp-api/src/main/java/com/misiontic/citapp/entity/Rol.java
@@ -1,32 +1,42 @@
 package com.misiontic.citapp.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name="Roles")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="roles")
 public class Rol {
 	
 	@Id
-	@Column(name="IdRol", nullable = false)
+	@Column(name="idrol", nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idRol;
 	
-	@Column(name="NombreRol", nullable = false, length = 100)
+	@Column(name="nombrerol", nullable = false, length = 100)
 	private String nombreRol;
 	
-	public Rol() {
-		
-	}
+	@OneToMany(mappedBy = "idRol", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Usuario> listaUsuarios;
 	
 	
 }
