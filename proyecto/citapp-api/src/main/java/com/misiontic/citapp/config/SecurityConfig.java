9c3668b2e0a671e6ef22c4e0d7a0a5c62e97e688
Fer <luferbrica@gmail.com>
2021-12-11 13:57:38 -0500

    se agregó SecurityConfig + paquete .security

--- proyecto/citapp-api/src/main/java/com/misiontic/citapp/config/SecurityConfig.java
+++ proyecto/citapp-api/src/main/java/com/misiontic/citapp/config/SecurityConfig.java
@@ -1,5 +1,47 @@
 package com.misiontic.citapp.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
-}
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	//@Bean
+	//public TokenAuthenticationFilter createTokenAuthenticationFilter() {
+	//	return new TokenAuthenticationFilter();
+	//}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.cors()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+		.csrf()
+			.disable()
+		.formLogin()
+			.disable()
+		.httpBasic()
+			.disable()
+		.exceptionHandling()
+			.authenticationEntryPoint(new RestAuthenticationEntryPoint())
+			.and()
+		.authorizeRequests()
+			.antMatchers(
+					//"/login",
+					//"/signUp"
+					)
+				.permitAll() 
+			.anyRequest()
+				.authenticated();
+		
+		http.addFilterBefore(createTokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+
